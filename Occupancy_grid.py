26c56e2827c9b9ecba1779a22bdd3d06db03a3f9
unknown <aditdesai4@gmail.com>
2020-12-17 23:45:25 -0800

    Added penalty for direction change

--- Occupancy_grid.py
+++ Occupancy_grid.py
@@ -14,19 +14,23 @@ class OccupancyGrid:
         self.width = top_right[0] - bottom_left[0]
 
     def get_neighbors(self, curr_pos):
+        dir_count = {(0, 0): 0, (0, 1): 1, (-1, 1): 2, (-1, 0): 3,
+                     (-1, -1): 4, (0, -1): 5, (1, -1): 6, (1, 0): 7, (1, 1): 8}  # might need a fix
         neighbors = []
-        x,y = curr_pos
+        neighbor_directions = []
+        x, y = curr_pos
         x_max, y_max = self.hi
         x_min, y_min = self.lo
         for i in [-1, 0, 1]:
             for j in [-1, 0, 1]:
-                x_new = x+i*self.resolution
-                y_new = y+j*self.resolution
+                x_new = x + i * self.resolution
+                y_new = y + j * self.resolution
                 new_pos = (x_new, y_new)
                 if x_max >= x_new >= x_min and y_max >= y_new >= y_min:
                     if new_pos not in self.obstacles:
                         neighbors.append(new_pos)
-        return neighbors
+                        neighbor_directions.append(dir_count[(i, j)])
+        return neighbors, neighbor_directions
 
     def set_obstacles(self, num_obstacles):
         # randomly choose the obstacle coordinates
@@ -38,16 +42,14 @@ class OccupancyGrid:
         ax = fig.add_subplot(111, aspect='equal')
 
         for obs in self.obstacles:
-            ax.add_patch(patches.Rectangle(obs, 1, 1, color = 'grey'))
+            ax.add_patch(patches.Rectangle(obs, 1, 1, color='grey'))
         for pos in path:
             ax.add_patch(patches.Rectangle(pos, 1, 1, color='red'))
         ax.add_patch(patches.Rectangle(start, 1, 1, color='blue'))
         ax.add_patch(patches.Rectangle(end, 1, 1, color='green'))
-        ticks = np.arange(0, self.height+1, self.resolution)
+        ticks = np.arange(0, self.height + 1, self.resolution)
         ax.set_xticks(ticks)
         ax.set_yticks(ticks)
         ax.set(xlim=(0, self.width), ylim=(0, self.height))
         plt.grid(linestyle="-", color='black')
         plt.show()
-
-
--- algorithms.py
+++ algorithms.py
@@ -9,12 +9,35 @@ class Node:
         self.data = data  # stores the node id number
         self.dist = math.inf  # stores distance from start node
         self.prev = None  # cache for the predecessor node in path
+        self.direction = None
 
     def __repr__(self):
         return f'Node data: {self.data}'
 
     def __lt__(self, other):
         return self.dist < other.dist
+
+# possible directions = [N S E W NE NW SE SW]
+# remember current direction
+# add direction change required from each node
+# penalize direction change
+
+
+def get_dir_penalty(curr_dir, new_dir):
+    change = abs(curr_dir - new_dir)
+    if change == 0:
+        penalty = 0
+    elif change in [1,7]:
+        penalty = 0.1
+    elif change in [2, 6]:
+        penalty = 0.15
+    elif change in [3, 5]:
+        penalty = 0.2
+    elif change == 4:
+        penalty = 0.25
+    else:
+        return False
+    return penalty
 
 
 def dijkstra(start_pos, end_pos, grid_obj):
@@ -24,7 +47,8 @@ def dijkstra(start_pos, end_pos, grid_obj):
     while len(queue) > 0:
         curr_pos = heapq.heappop(queue)
         if curr_pos.data not in discovered:
-            print(curr_pos)
+            # print(curr_pos)
+            curr_dir = curr_pos.direction
             discovered.add(curr_pos.data)
             if curr_pos.data == end_pos.data:
                 path.append(curr_pos.data)
@@ -34,23 +58,26 @@ def dijkstra(start_pos, end_pos, grid_obj):
                     curr_pos = previous
                 print(path)
                 return path
-            neighbors = grid_obj.get_neighbors(curr_pos.data)
-            for pos in neighbors:
+            neighbors, neighbor_directions = grid_obj.get_neighbors(curr_pos.data)
+            for i,pos in enumerate(neighbors):
                 pos_node = Node(pos)
-                pos_node.dist = curr_pos.dist + 1
+                pos_node.direction = neighbor_directions[i]
+                print(pos_node.direction)
+                penalty = get_dir_penalty(curr_dir, pos_node.direction)
+                pos_node.dist = curr_pos.dist + 1 + penalty
                 pos_node.prev = curr_pos
                 heapq.heappush(queue, pos_node)
         else:
-            print('ha')
             continue
     return False
 
 bottom_left = (0,0)
 top_right = (20,20)
 resolution = 1
-obstacles = [(1,5),(2,5),(3,5),(4,5),(5,5),(6,5),(7,5),(7,6)]
+obstacles = [(1,5),(2,5),(3,5),(4,5),(5,5),(6,5),(7,5),(7,6), (4,5), (4,6), (4,7)]
 grid = OccupancyGrid(bottom_left, top_right, resolution, obstacles)
 start_pos = Node((0,0))
 start_pos.dist = 0
+start_pos.direction = 90
 path = dijkstra(start_pos, Node((5,7)), grid)
 grid.plot_grid(start_pos.data, (5,7), path)
