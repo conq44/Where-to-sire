c2ad0dc4605be12553955000a75b3a023b9ad24a
unknown <aditdesai4@gmail.com>
2021-01-11 14:28:53 -0800

    Cleaned up plotting function

--- Occupancy_grid.py
+++ Occupancy_grid.py
@@ -17,11 +17,18 @@ class OccupancyGrid:
         x_max, y_max = self.hi
         x_min, y_min = self.lo
         x_new, y_new = new_pos
+        is_free = True
         if x_max >= x_new >= x_min and y_max >= y_new >= y_min:
-            if new_pos not in self.obstacles:
-                return True
-            else:
-                return False
+            for obs in self.obstacles:
+                x_o, y_o = obs[0]
+                height = obs[1]
+                width = obs[2]
+                if x_o + width - 1 >= x_new >= x_o and y_o + height - 1 >= y_new >= y_o:
+                    is_free = False
+                    return is_free
+        else:
+            is_free = False
+        return is_free
 
     def get_neighbors(self, curr_pos):
         dir_count = {(0, 0): 0, (0, 1): 1, (-1, 1): 2, (-1, 0): 3,
@@ -48,18 +55,25 @@ class OccupancyGrid:
         fig = plt.figure()
         ax = fig.add_subplot(111, aspect='equal')
         for point in discovered:
-            ax.add_patch(patches.Rectangle(point, 1, 1, color='yellow'))
+            ax.add_patch(patches.Rectangle(point, 1, 1, color='yellow', label='Discovered'))
         for obs in self.obstacles:
-            ax.add_patch(patches.Rectangle(obs, 1, 1, color='grey'))
+            loc = obs[0]
+            height = obs[1]
+            width = obs[2]
+            ax.add_patch(patches.Rectangle(loc, width, height, color='grey', label='Obstacle'))
         for pos in path:
-            ax.add_patch(patches.Rectangle(pos, 1, 1, color='red'))
-        ax.add_patch(patches.Rectangle(start, 1, 1, color='blue'))
-        ax.add_patch(patches.Rectangle(end, 1, 1, color='green'))
-        ticks = np.arange(0, self.height + 1, self.resolution)
-        ax.set_xticks(ticks)
-        ax.set_yticks(ticks)
+            ax.add_patch(patches.Rectangle(pos, 1, 1, color='red', label='Path'))
+        ax.add_patch(patches.Rectangle(start, 1, 1, color='blue', label='Start'))
+        ax.add_patch(patches.Rectangle(end, 1, 1, color='green', label='Goal'))
         ax.set(xlim=(0, self.width), ylim=(0, self.height))
         plt.grid(linestyle="-", color='black')
+        handles, labels = plt.gca().get_legend_handles_labels()
+        newLabels, newHandles = [], []
+        for handle, label in zip(handles, labels):
+            if label not in newLabels:
+                newLabels.append(label)
+                newHandles.append(handle)
+        plt.legend(newHandles, newLabels, loc='upper right')
         plt.show()
 
 
@@ -94,20 +108,27 @@ class OccupancyGridRRT:
         end_region = (xe -1, ye -1)
         fig = plt.figure()
         ax = fig.add_subplot(111, aspect='equal')
-        # for point in discovered:
-        #     x, y = point
-        #     plt.plot(x, y, 'yo')
+        for point in discovered:
+            x, y = point
+            plt.plot(x, y, 'yo', markersize = 1)
         for line in self.obstacles:
-            self.plot_line(line, color='grey')
+            self.plot_line(line, color='grey', label='Obstacles')
         for line in self.lines:
-            self.plot_line(line, color='blue', linewidth=0.5)
+            self.plot_line(line, color='blue', linewidth=0.5, label='Tree')
         for i in range(len(path)-1):
             line = [path[i], path[i+1]]
-            self.plot_line(line, color='black')
+            self.plot_line(line, color='black', label='Basic path')
         for i in range(len(shortcut_path)-1):
             line = [shortcut_path[i], shortcut_path[i+1]]
-            self.plot_line(line, color='red')
+            self.plot_line(line, color='red', label='Shortcut path')
         ax.add_patch(patches.Rectangle(start, 1, 1, color='blue'))
         ax.add_patch(patches.Rectangle(end_region, 2, 2, color='green'))
         ax.set(xlim=(0, self.width), ylim=(0, self.height))
+        handles, labels = plt.gca().get_legend_handles_labels()
+        newLabels, newHandles = [], []
+        for handle, label in zip(handles, labels):
+            if label not in newLabels:
+                newLabels.append(label)
+                newHandles.append(handle)
+        plt.legend(newHandles, newLabels, loc='upper right')
         plt.show()
